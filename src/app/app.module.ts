dc7fc1a9e62c7a24f761abd80e5221f548c5b605
Ayman Hussein <ayman.kanaaneh@gmail.com>
2023-10-27 01:15:51 +0300

    feat: design conatct feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
 import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
@@ -24,6 +25,7 @@ import { WorkComponent } from './work/work.component';
     HomeComponent,
     SkillsComponent,
     WorkComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
