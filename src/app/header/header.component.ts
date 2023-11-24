e8b7216b4e1ce82f9ec308a3f9a996a80b9a7805
Ayman Hussein <ayman.kanaaneh@gmail.com>
2023-11-25 01:35:36 +0200

    feat: add router pagination

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,9 @@
-import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements AfterViewInit{
-
-  @ViewChild('home', { static: true }) homeElement!: ElementRef<HTMLElement>;
-  @Output() scroll = new EventEmitter<string>();
-  
-  ngAfterViewInit(): void {
-    this.homeElement.nativeElement.focus();
-  }
-
-  scrollTo(value: string){
-    this.scroll.emit(value);
-  }
-
+export class HeaderComponent {
 }
